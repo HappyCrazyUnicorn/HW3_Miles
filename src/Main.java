8e81261385e1c6b44539f063ce116ba589d22d49
HappyCrazyUnicorn <pankova.mary.v@gmail.com>
2022-03-26 15:21:11 +0300

    Reformat code, also changed println

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+
         BonusMilesService service = new BonusMilesService(); //создаем объект из класса BonusMilesService
         int price = 10_000; //объявляем переменную стоимости билета
         int miles = service.calculate(price); //вызываем метод calculate из класса BonusMilesService, передаем на вход стоимость билета
-        System.out.println(miles); //выводим рассчетное количество миль
+
+        System.out.println("Вам будет начислено " + miles + " миль"); //выводим рассчетное количество миль
     }
 }
